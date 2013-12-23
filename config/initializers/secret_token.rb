f1b1a54fc7312b70c314c138fc2e11ae3437c032
Dontae Rayford <dontae.rayford@gmail.com>
2013-12-22 22:43:40 -0500

    initial commit of albums project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Albums::Application.config.secret_key_base = '7dbcb5ae5d3d1dc9883be118ad5030ea59a2554b6fc7576d820ef050484020a9f59ac29f28545368a4289b5ec73d8b1c7cb2fb309463e4cf59978604ba97e2d3'
